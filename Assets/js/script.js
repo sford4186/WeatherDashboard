b8fcd18e995e3f8a7e6514b33e6cb4d2001d9ea8
Shawanda Ford <shawanda.ford@gmail.com>
2020-08-20 23:53:56 -0400

    added weather icons to 5 day forecast

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -94,8 +94,8 @@ $(document).ready(function () {
       for (i = 0; i < response.list.length; i++) {
         var curr = response.list[i]
         if (curr.dt_txt.includes("12:00")) {
-          var newDiv = $("<div class='card bg-primary text-light col-md-3'>")
-          newDiv.append(moment.unix(curr.dt).format("L")+"<div> Temp: "+response.list[i].main.temp +"</div>"+"<div> Humidity: "+response.list[i].main.humidity+"%</div>")
+          var newDiv = $("<div class='card bg-primary text-light col-md-2'>")
+          newDiv.append(moment.unix(curr.dt).format("L")+ '<img src="' +'http://openweathermap.org/img/w/' + response.list[i].weather[0].icon + '.png' + '" alt="weather icon">' + '</div>'+"<div> Temp: "+response.list[i].main.temp +"</div>"+"<div> Humidity: "+response.list[i].main.humidity+"%</div>")
           
           $(".fiveDay").append(newDiv)
           console.log(response.list[i].weather[0].icon)
@@ -110,69 +110,5 @@ $(document).ready(function () {
   })
 
 
-  //   let unix_timestamp = 1549312452
-  // // Create a new JavaScript Date object based on the timestamp
-  // // multiplied by 1000 so that the argument is in milliseconds, not seconds.
-  // var date = new Date(unix_timestamp * 1000);
-  // // Hours part from the timestamp
-  // var hours = date.getHours();
-  // // Minutes part from the timestamp
-  // var minutes = "0" + date.getMinutes();
-  // // Seconds part from the timestamp
-  // var seconds = "0" + date.getSeconds();
-
-  // // Will display time in 10:30:23 format
-  // var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
-
-  // console.log(formattedTime);
-  //   console.log(queryURL)
-  //   console.log(response)
-  //   console.log(response.list[i].main.humidity)
-  //   console.log(response.list[i].wind.speed)
-
-  //renderSearch()
-
-  // });
-
-  // // Function for displaying movie data
-  // function renderSearch() {
-
-  // // Deleting the movie buttons prior to adding new movie buttons
-  // // (this is necessary otherwise we will have repeat buttons)
-  // $("#city-status").empty();
-
-  // // Looping through the array of movies
-  // for (var i = 0; i < cities.length; i++) {
-
-  // // Then dynamicaly generating buttons for each movie in the array.
-  // // This code $("<button>") is all jQuery needs to create the start and end tag. (<button></button>)
-  // var list = $("<p>");
-  // // Adding a class
-  // list.addClass("cities");
-  // // Adding a data-attribute with a value of the movie at index i
-  // list.attr("data-name", cities[i]);
-  // // Providing the button's text with a value of the movie at index i
-  // list.text(cities[i]);
-  // // Adding the button to the HTML
-  // $("#city-status").append(list);
-  // }
-  // }
-
-  // // This function handles events where one button is clicked
-  // $("#city-status").on("click", function(event) {
-  //   // event.preventDefault() prevents the form from trying to submit itself.
-  //   // We're using a form so that the user can hit enter instead of clicking the button if they want
-  //   event.preventDefault();
-
-  //   // This line will grab the text from the input box
-  //   var city = $("#city-input").val().trim();
-  //   // The movie from the textbox is then added to our array
-  //   cities.push(city);
-  // $("city-status").append(cities)
-  //   // calling renderButtons which handles the processing of our movie array
-  //   renderSearch();
-  // });
-  // renderSearch()
-
-  // });
+  
 });
